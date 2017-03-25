395d9d8bddfd56b4762f831311f5c6b2dafcfa78
kbazant <krizz100@yahoo.com>
2017-03-25 23:19:56 +0000

    moved the "counting" function from index.html to timer.js file

---
+++ timer.js
@@ -0,0 +1,25 @@
+function counting() {
+
+	let today = new Date();
+	let day = today.getDate();
+	if (day < 10) day = "0" + day;
+
+	let month = today.getMonth() + 1;
+	if (month < 10) month = "0" + month;
+
+	let year = today.getFullYear();
+
+	let hour = today.getHours();
+	if (hour < 10) hour = "0" + hour;
+
+	let minute = today.getMinutes();
+	if (minute < 10) minute = "0" + minute;
+
+	let second = today.getSeconds();
+	if (second < 10) second = "0" + second;
+
+	document.getElementById("clock").innerHTML =
+		day + "/" + month + "/" + year + " | " + hour + ":" + minute + ":" + second;
+
+	setTimeout("counting()", 1000);
+}
