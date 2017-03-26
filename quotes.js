34045b3d810ece804e788d370a76765c099470a8
kbazant <krizz100@yahoo.com>
2017-03-26 14:50:47 +0100

    increased number of quotes to 5

--- quotes.js
+++ quotes.js
@@ -2,8 +2,20 @@
 
 $(document).ready(function () {
 	function getQuote() {
-		var quotes = ["Quote 1", "Quote 2", "Quote 3"];
-		var author = ["Author 1", "Author 2", "Author 3"];
+		var quotes = [
+			"“How did it get so late so soon?”", /*quote 1*/
+			"“Time is the longest distance between two places.”", /*quote 2*/
+			"“You may delay, but time will not.”", /*quote 3*/
+			"“Time is what we want most,but what we use worst.”", /*quote 4*/
+			"“Don’t waste your time in anger, regrets, worries, and grudges. Life is too short to be unhappy.”" /*quote 5*/
+		];
+		var author = [
+			"Dr. Seuss", /*author 1*/
+			"Tennessee Williams", /*author 2*/
+			"Benjamin Franklin", /*author 3*/
+			"William Penn", /*author 4*/
+			"Roy T. Bennett" /*author 5*/
+		];
 
 		var randomNum = Math.floor((Math.random() * quotes.length));
 		var randomQuote = quotes[randomNum];
