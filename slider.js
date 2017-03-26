94c9aecf0d8b818c8971fdfda48e64ba4d182f6f
kbazant <krizz100@yahoo.com>
2017-03-26 20:58:01 +0100

    added setInterval function

--- slider.js
+++ slider.js
@@ -2,7 +2,25 @@ var imagecount = 1;
 var total = 5;
 
 function slide(x) {
-	var Image = document.getElementsByClassName('img');
-	imagecount = imagecount + 1;
-	Image.src = "images/img" + imagecount +".jpg";
+	var Image = document.getElementsById('img');
+	imagecount = imagecount + x;
+	if (imagecount > total) {
+		imagecount = 1;
+	}
+	if (imagecount < 1) {
+		imagecount = total;
+	}
+	Image.src = "images/img" + imagecount + ".jpg";
 }
+
+function.setInterval(function slideA(x) {
+	var Image = document.getElementsById('img');
+	imagecount = imagecount + x;
+	if (imagecount > total) {
+		imagecount = 1;
+	}
+	if (imagecount < 1) {
+		imagecount = total;
+	}
+	Image.src = "images/img" + imagecount + ".jpg";
+}, 5000);
