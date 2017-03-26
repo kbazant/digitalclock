2f9ff5cc3f099432e956bb82429ff6e400828918
kbazant <krizz100@yahoo.com>
2017-03-26 11:49:14 +0100

    added Math.random for quotes and author

--- timer.js
+++ timer.js
@@ -30,7 +30,16 @@ function counting() {
 
 $(document).ready(function(){
 	function getQuote(){
-		const quotes = ["Quote 1", "Quote 2", "Quote 3"];
-		const authors  = ["Author 1", "Author 2", "Author 3"];
+		var quotes = ["Quote 1", "Quote 2", "Quote 3"];
+		var author = ["Author 1", "Author 2", "Author 3"];
+		
+		var randomNum = Math.floor((Math.random()*quotes.length));
+		var randomQuote = quotes[randomNum];
+		var randomAuthor = author[randomNum];
+		
+		$(".quote-text").text(randomQuote);
+		$(".quote-author").text(randomAuthor);
 	}
+	
+	
 });
